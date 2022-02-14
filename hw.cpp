f2f7704bc3123a950311c59617d2882af53aee43
Deeksha <deekshad1502@gmail.com>
2022-02-14 17:07:14 +0530

    first commit

---
+++ hw.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+int main()
+{
+
+    int a,b,sum;
+
+    std::cout<<"Enter two numbers";
+    std::cin>>a>>b;
+    sum=a+b;
+    std::cout<<"The sum is"<<sum<<std::endl;
+
+}
