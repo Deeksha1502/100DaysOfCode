6b36fc00519ee3a1805e2fe46808ee142c810466
Deeksha <deekshad1502@gmail.com>
2022-02-15 19:03:54 +0530

    Day 2

--- convert_seconds_into_hrs_mins_secs.cpp
+++ convert_seconds_into_hrs_mins_secs.cpp
@@ -4,6 +4,13 @@ int main()
 {
     int total_seconds,minutes,hours,seconds;
     std::cout<<"Enter the total seconds";
-    std::cin>>total_seconds;
-    
+    std::cin>>seconds;
+    total_seconds=seconds;
+    hours=seconds/3600;
+    seconds=seconds%3600;
+    minutes=seconds/60;
+    seconds=seconds%60;
+   
+std::cout<<total_seconds<<" seconds= "<<hours<<" hour "<<minutes<<" minutes "<<seconds<<" seconds "<<std::endl;
+
 }
